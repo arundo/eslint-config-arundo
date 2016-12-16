830424884d48e9539adb1ea37beebfffeadeb628
Tucker Cowie <tuckercowie@gmail.com>
2016-12-16 14:18:07 -0600

    Remove top-level airbnb dependency

--- index.js
+++ index.js
@@ -15,10 +15,10 @@
 */
 
 module.exports = {
-    extends: [ 'airbnb'],
     rules: {
         "indent": [ 2, 2, { "SwitchCase": 1} ],
         "space-before-blocks": [2, "never"],
         "max-len": [1, 120, 4]
     },
+  extends: ['airbnb-base'],
 };
